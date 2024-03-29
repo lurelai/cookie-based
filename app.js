2c2d88646315d84a1964a7cd060f0e491dcbd109
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-03-29 13:44:50 -0300

    saving sessions

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const app = express()
+const { randomUUID } = require('node:crypto')
 
 // Connect with database
-const { createConnection } = require('./src/database/db')
+const { createConnection, query } = require('./src/database/db')
 
 createConnection().then(async (time)=>{
 	console.log("Connection time: " + time.connectionTime + "ms")
 }).catch(err=>{ console.log(err) })
+
+// Allow body-parser
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 
 app.get('/', (req, res)=>{
 	return res.send(`
@@ -19,7 +24,16 @@ app.get('/', (req, res)=>{
 })
 
 app.post('/login', (req, res)=>{
-	return res.send({okay: 'okay'})
+	// Possibles [{id: 1, password: mama}, {id: 2, password: mia}, {id: 3, password: figaro}]
+	const { id, password } = req.body
+
+	if((id === "1" && password === "mama") || (id === "2" && password === "mia") || (id === "3" && password === "figaro")){
+		query("INSERT INTO sessions(user_id, user_session) VALUES($1, $2)", [id, randomUUID()])
+
+		return res.send("logged")
+	}
+
+	return res.send("not logged")
 })
 
 app.listen(8000)
